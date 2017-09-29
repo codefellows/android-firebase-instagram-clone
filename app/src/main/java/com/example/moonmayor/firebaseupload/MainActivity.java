0e7e9801c1fc1e6c95876d18dbd522a163cf2b9b
Steve Geluso <stevegeluso@gmail.com>
2017-09-29 15:17:04 -0700

    retrieving text list of photo URLs

--- app/src/main/java/com/example/moonmayor/firebaseupload/MainActivity.java
+++ app/src/main/java/com/example/moonmayor/firebaseupload/MainActivity.java
@@ -11,10 +11,16 @@ import android.os.Bundle;
 import android.support.v7.widget.DrawableUtils;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
@@ -25,8 +31,10 @@ import java.io.File;
 public class MainActivity extends AppCompatActivity {
 
     private StorageReference mStorageRef;
+    private FirebaseDatabase mDB;
 
     TextView mMessage;
+    TextView mMessage2;
     Button mUploadButton;
 
     @Override
@@ -35,8 +43,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mStorageRef = FirebaseStorage.getInstance().getReference();
-
+        mDB = FirebaseDatabase.getInstance();
         mMessage = (TextView) findViewById(R.id.message);
+        mMessage2 = (TextView) findViewById(R.id.message2);
         mUploadButton = (Button) findViewById(R.id.upload);
 
         mUploadButton.setOnClickListener(new View.OnClickListener() {
@@ -45,6 +54,8 @@ public class MainActivity extends AppCompatActivity {
                 upload();
             }
         });
+
+        loadPictures();
     }
 
     private void upload() {
@@ -62,6 +73,8 @@ public class MainActivity extends AppCompatActivity {
                 // Get a URL to the uploaded content
                 Uri downloadUrl = taskSnapshot.getDownloadUrl();
                 mMessage.setText(downloadUrl.toString());
+
+                addPhotoToList(downloadUrl.toString());
             }
         })
         .addOnFailureListener(new OnFailureListener() {
@@ -70,5 +83,86 @@ public class MainActivity extends AppCompatActivity {
                 mMessage.setText("Error: " + exception.getMessage());
             }
         });
+    }
+
+    private void loadPictures() {
+        DatabaseReference photoRef = mDB.getReference().child("photos");
+        photoRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                int i = 0;
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                    int id = 0;
+                    if (i == 0) {
+                        id = R.id.message;
+                    } else if (i == 1) {
+                        id = R.id.message2;
+                    } else if (i == 2) {
+                        id = R.id.message3;
+                    }
+
+                    if (id == R.id.message || id == R.id.message2 || id == R.id.message3) {
+                        TextView text = (TextView) findViewById(id);
+                        text.setText(snapshot.getValue(String.class));
+                    }
+                    i++;
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+    }
+
+    private void addPhotoToList(String url) {
+        DatabaseReference photoRef = mDB.getReference().child("photos");
+
+        DatabaseReference pushRef = photoRef.push();
+        pushRef.setValue(url);
+
+        pushRef = photoRef.push();
+        pushRef.setValue(url + "2");
+
+        pushRef = photoRef.push();
+        pushRef.setValue(url + "3");
+    }
+
+    private void attachDBListeners() {
+        DatabaseReference myRef = mDB.getReference().child("message");
+        myRef.setValue("what up");
+        myRef.push();
+
+        DatabaseReference myRef2 = mDB.getReference().child("message2");
+        myRef2.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                String val = dataSnapshot.getValue(String.class);
+                mMessage2.setText("message changed to: " + val);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                String val = dataSnapshot.getValue(String.class);
+                mMessage.setText("message changed to: " + val);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+    private void listFiles() {
     }
 }
