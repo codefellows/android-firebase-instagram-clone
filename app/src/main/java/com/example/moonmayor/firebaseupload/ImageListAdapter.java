874fc19898cf672b3468ca66d8728516c7ed3aaf
Steve Geluso <stevegeluso@gmail.com>
2017-09-30 12:33:28 -0700

    adding instagram-like flavor text and heart

--- app/src/main/java/com/example/moonmayor/firebaseupload/ImageListAdapter.java
+++ app/src/main/java/com/example/moonmayor/firebaseupload/ImageListAdapter.java
@@ -2,6 +2,7 @@ package com.example.moonmayor.firebaseupload;
 
 import android.content.Context;
 import android.database.DataSetObserver;
+import android.graphics.drawable.Drawable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -20,16 +21,22 @@ import java.util.List;
 
 public class ImageListAdapter extends ArrayAdapter<String> {
 
+    Context mContext;
+
     private List<String> mUrls;
     private ViewHolder holder;
 
     public class ViewHolder {
-        TextView url;
+        ImageView heart;
+        TextView likes;
+        TextView author;
+        TextView timestamp;
         ImageView image;
     }
 
     public ImageListAdapter(Context context, int resource, List<String> urls) {
         super(context, resource, urls);
+        mContext = context;
         mUrls = urls;
     }
 
@@ -45,14 +52,39 @@ public class ImageListAdapter extends ArrayAdapter<String> {
         }
 
         holder = new ViewHolder();
-        holder.url = convertView.findViewById(R.id.url);
+        holder.heart = convertView.findViewById(R.id.heart);
+        holder.likes = convertView.findViewById(R.id.likes);
+        holder.author = convertView.findViewById(R.id.author);
+        holder.timestamp = convertView.findViewById(R.id.timestamp);
         holder.image = convertView.findViewById(R.id.image);
 
+        configureHeartToggler();
+
+        holder.likes.setText("11 likes");
+        holder.author.setText("slothprovider @everyone here's more sloths for you!");
+        holder.timestamp.setText("54 minutes ago");
 
         String url = getItem(i);
-        holder.url.setText(url);
-
         new LoadImageTask(url, holder.image).execute();
         return convertView;
+    }
+
+    private void configureHeartToggler() {
+        final Drawable emptyHeart = mContext.getResources().getDrawable(R.drawable.insta_heart_empty);
+        final Drawable fullHeart = mContext.getResources().getDrawable(R.drawable.insta_heart_full);
+        holder.heart.setImageDrawable(emptyHeart);
+
+        holder.heart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ImageView image = (ImageView) view;
+                if (image.getDrawable() == emptyHeart) {
+                    holder.heart.setImageDrawable(fullHeart);
+                } else {
+                    holder.heart.setImageDrawable(emptyHeart);
+                }
+            }
+        });
+
     }
 }
