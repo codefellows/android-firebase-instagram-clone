3f796c404e07870e8a90fd5e6d1890dc7f9b8aac
Steve Geluso <stevegeluso@gmail.com>
2017-09-30 11:23:11 -0700

    adding camera. new xml file, configuring provider in manifest

--- app/src/main/java/com/example/moonmayor/firebaseupload/MainActivity.java
+++ app/src/main/java/com/example/moonmayor/firebaseupload/MainActivity.java
@@ -1,13 +1,17 @@
 package com.example.moonmayor.firebaseupload;
 
 import android.content.Context;
+import android.content.Intent;
 import android.database.DataSetObserver;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.AsyncTask;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.annotation.NonNull;
+import android.support.v4.content.FileProvider;
 import android.support.v4.content.res.ResourcesCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -36,10 +40,15 @@ import java.io.File;
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private final int REQUEST_IMAGE_CAPTURE = 1;
+    private String mCurrentPhotoPath;
 
     Context mContext;
 
@@ -47,6 +56,7 @@ public class MainActivity extends AppCompatActivity {
     private FirebaseDatabase mDB;
 
     TextView mMessage;
+    Button mTakePictureButton;
     Button mUploadButton;
 
     private List<String> imageUrls;
@@ -62,7 +72,21 @@ public class MainActivity extends AppCompatActivity {
         mStorageRef = FirebaseStorage.getInstance().getReference();
         mDB = FirebaseDatabase.getInstance();
         mMessage = (TextView) findViewById(R.id.message);
+        mTakePictureButton = (Button) findViewById(R.id.takePicture);
         mUploadButton = (Button) findViewById(R.id.upload);
+
+
+        attachClickListeners();
+        loadPictures();
+    }
+
+    private void attachClickListeners() {
+        mTakePictureButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                takePicture();
+            }
+        });
 
         mUploadButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -70,8 +94,33 @@ public class MainActivity extends AppCompatActivity {
                 upload();
             }
         });
+    }
 
-        loadPictures();
+    private void takePicture() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            // Create the File where the photo should go
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+                // Error occurred while creating the File
+                mMessage.setText("Error: " + ex.getMessage());
+            }
+            // Continue only if the File was successfully created
+            if (photoFile != null) {
+                Uri photoURI = FileProvider.getUriForFile(this,
+                        "com.example.moonmayor.firebaseupload",
+                        photoFile);
+                // Including this option tells the Intent to write the photo result
+                // to a file location, and it does not return an image thumbnail.
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+
+                // add the picture to the phone's gallery
+                //galleryAddPic();
+            }
+        }
     }
 
     private void upload() {
@@ -160,5 +209,22 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void listFiles() {
+    }
+
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        mCurrentPhotoPath = image.getAbsolutePath();
+        return image;
     }
 }
