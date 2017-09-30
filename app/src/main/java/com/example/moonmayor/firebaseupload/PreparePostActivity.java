fc9f2cd7141a4bc442c58a2618bd794bc82febb6
Steve Geluso <stevegeluso@gmail.com>
2017-09-30 15:00:34 -0700

    displaying image preview in PreparePost

--- app/src/main/java/com/example/moonmayor/firebaseupload/PreparePostActivity.java
+++ app/src/main/java/com/example/moonmayor/firebaseupload/PreparePostActivity.java
@@ -2,11 +2,14 @@ package com.example.moonmayor.firebaseupload;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 
 /**
  * Created by moonmayor on 9/30/17.
@@ -18,6 +21,7 @@ public class PreparePostActivity extends AppCompatActivity {
 
     String mFilepath;
 
+    ImageView mImage;
     Button mCancel;
     Button mPost;
 
@@ -25,10 +29,12 @@ public class PreparePostActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.prepare_post);
 
-        mFilepath = getIntent().getStringExtra(EXTRA_FILEPATH);
-
+        mImage = (ImageView) findViewById(R.id.preview);
         mCancel = (Button) findViewById(R.id.cancel);
         mPost = (Button) findViewById(R.id.post);
+
+        mFilepath = getIntent().getStringExtra(EXTRA_FILEPATH);
+        setPicFromFile(mFilepath);
 
         attachClickHandlers();
     }
@@ -56,5 +62,33 @@ public class PreparePostActivity extends AppCompatActivity {
                 finish();
             }
         });
+    }
+
+    private void setPicFromFile(String filepath) {
+        // Get the dimensions of the View
+        int targetW = mImage.getWidth();
+        int targetH = mImage.getHeight();
+
+        // Get the dimensions of the bitmap
+        BitmapFactory.Options bmOptions = new BitmapFactory.Options();
+        bmOptions.inJustDecodeBounds = true;
+        BitmapFactory.decodeFile(filepath, bmOptions);
+        int photoW = bmOptions.outWidth;
+        int photoH = bmOptions.outHeight;
+
+        // prevent divide by zero errors
+        targetW = targetW == 0 ? photoW : targetW;
+        targetH = targetH == 0 ? photoH : targetH;
+
+        // Determine how much to scale down the image
+        int scaleFactor = Math.min(photoW/targetW, photoH/targetH);
+
+        // Decode the image file into a Bitmap sized to fill the View
+        bmOptions.inJustDecodeBounds = false;
+        bmOptions.inSampleSize = scaleFactor;
+        bmOptions.inPurgeable = true;
+
+        Bitmap bitmap = BitmapFactory.decodeFile(filepath, bmOptions);
+        mImage.setImageBitmap(bitmap);
     }
 }
